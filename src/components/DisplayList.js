2805c33f2236674c2d27906956d7c91e254c9d88
Lee Odell <leerodell@gmail.com>
2019-04-01 17:00:37 -0700

    commit five

--- src/components/DisplayList.js
+++ src/components/DisplayList.js
@@ -10,8 +10,8 @@ class DisplayList extends Component {
             <div>
                 {this.props.task.map((element,index)=>{
                 return <h4 key={index}>{element}
-                <button className="completeButton" onClick={()=>{this.props.handleComplete(index)}}>complete</button>
-                <button className="deleteButton"onClick={()=>{this.props.handleDelete(index)}}>delete</button>
+                <button className="completeButton" onClick={()=>{this.props.handleComplete(index)}}>COMPLETE</button>
+                <button className="deleteButton"onClick={()=>{this.props.handleDelete(index)}}>REMOVE</button>
                 </h4>
               
                 })}
